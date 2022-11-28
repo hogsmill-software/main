9963fe052a0e78800b6ff2ad95d6086fdbce15b0
ChristopherCamacho <hogsmill.software@gmail.com>
2022-11-28 15:14:20 +0000

    adjust translucide background
    mustn't be too light or not obvious background is inactive and mustn't be too dark or not obvious background is still there.

--- java/com/example/frametext/userControls/colorPicker/ColorPickerPopup.kt
+++ java/com/example/frametext/userControls/colorPicker/ColorPickerPopup.kt
@@ -54,7 +54,7 @@ class ColorPickerPopup private constructor(builder: Builder) {
             blankLayout, ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT
         )
-        popupWindowDarkBackground!!.setBackgroundDrawable(ColorDrawable(0x30000000))
+        popupWindowDarkBackground!!.setBackgroundDrawable(ColorDrawable(0x50000000))
         popupWindowDarkBackground!!.isOutsideTouchable =
             false // so doesn't close if click on outside
         colorPickerView.setInitialColor(initialColor)
